f856d98819084602c73d2f1c40ea7ec7517a6a54
gaojude <gaojude@outlook.com>
2021-01-02 15:35:55 -0500

    some fix & modify

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
-import './index.css'
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./index.css";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-import {HashRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
-    </Provider>, document.getElementById('root'))
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
