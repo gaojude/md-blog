ccd01eb7934ec790962c08647313fc342d6a5d95
gaojude <gaojude@outlook.com>
2021-01-02 21:29:15 -0500

    new build mechanics

---
+++ server/public/precache-manifest.164a911b0618c048a5de026a8a4d58d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311e9750447c8b07f06e0d0cb0590645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8251a2c4cc266699fae",
+    "url": "/static/css/main.952a1793.chunk.css"
+  },
+  {
+    "revision": "cc7e41f19dc688d1052b",
+    "url": "/static/js/2.79545bbe.chunk.js"
+  },
+  {
+    "revision": "0c97063bb9bb687cd8ff95f9d589cd5f",
+    "url": "/static/js/2.79545bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8251a2c4cc266699fae",
+    "url": "/static/js/main.984a0966.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
